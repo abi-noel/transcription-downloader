63a6994fae5aa134f63ec8e6f166d38aefeca98d
abi-noel <abi_noel7@yahoo.com>
2023-12-27 21:34:52 -0500

    this doesn't do anything but at least it compiles and runs

--- project/src/main/java/com/transcriptiondownloader/TranscriptionDownloader.java
+++ project/src/main/java/com/transcriptiondownloader/TranscriptionDownloader.java
@@ -1,99 +1,49 @@
 package com.transcriptiondownloader;
 
-/**
- * Sample Java code for youtube.captions.download
- * See instructions for running these code samples locally:
- * https://developers.google.com/explorer-help/code-samples#java
- */
-
-import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
-import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
-import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
-import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
-import com.google.api.client.googleapis.json.GoogleJsonResponseException;
 import com.google.api.client.http.javanet.NetHttpTransport;
-import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
-
 import com.google.api.services.youtube.YouTube;
+import com.google.api.services.youtube.model.Caption;
+import com.google.api.services.youtube.model.CaptionListResponse;
 
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.security.GeneralSecurityException;
-import java.util.Arrays;
-import java.util.Collection;
+import java.util.List;
 
 public class TranscriptionDownloader {
-    private static final String CLIENT_SECRETS= "/client_secret.json";
-    private static final Collection<String> SCOPES =
-        Arrays.asList("https://www.googleapis.com/auth/youtube.force-ssl");
+    private static final String API_KEY = "AIzaSyAzuKQQeiCJBgxgHeu1AqbvyERkgARlAAw"; // Replace with your actual API key
 
-    private static final String APPLICATION_NAME = "API code samples";
-    private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
-
-    /**
-     * Create an authorized Credential object.
-     *
-     * @return an authorized Credential object.
-     * @throws IOException
-     */
-    public static Credential authorize(final NetHttpTransport httpTransport) throws IOException {
-        // Load client secrets.
-        InputStream in = TranscriptionDownloader.class.getResourceAsStream(CLIENT_SECRETS);
-        if (in == null) {
-            throw new IOException("Resource not found: " + CLIENT_SECRETS);
-        }
-        GoogleClientSecrets clientSecrets =
-          GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
-        // Build flow and trigger user authorization request.
-        GoogleAuthorizationCodeFlow flow =
-            new GoogleAuthorizationCodeFlow.Builder(httpTransport, JSON_FACTORY, clientSecrets, SCOPES)
-            .build();
-
-        int fixedPort = 7777;
-        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(fixedPort).build();
-
-        Credential credential =
-            new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
-        return credential;
-    }
-
-    /**
-     * Build and return an authorized API client service.
-     *
-     * @return an authorized API client service
-     * @throws GeneralSecurityException, IOException
-     */
-    public static YouTube getService() throws GeneralSecurityException, IOException {
-        final NetHttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
-        Credential credential = authorize(httpTransport);
-        return new YouTube.Builder(httpTransport, JSON_FACTORY, credential)
-            .setApplicationName(APPLICATION_NAME)
-            .build();
-    }
-
-    /**
-     * Call function to create API service object. Define and
-     * execute API request. Print API response.
-     *
-     * @throws GeneralSecurityException, IOException, GoogleJsonResponseException
-     */
-    public static void main(String[] args)
-        throws GeneralSecurityException, IOException, GoogleJsonResponseException {
+    public static void main(String[] args) throws GeneralSecurityException, IOException {
         YouTube youtubeService = getService();
-        // TODO: Replace "YOUR_FILE" with the location where
-        //       the downloaded content should be written.
-        OutputStream output = new FileOutputStream("output.txt");
 
-        // Define and execute the API request
-        YouTube.Captions.Download request = youtubeService.captions()
-            .download("CcG9boSS04s");
-        request.getMediaHttpDownloader();
-        request.executeMediaAndDownloadTo(output);
+        // TODO: Replace "VIDEO_ID" with the ID of the YouTube video you want to download captions for
+        String videoId = "CcG9boSS04s";
+        listCaptions(youtubeService, videoId);
+    }
+
+    public static YouTube getService() throws GeneralSecurityException, IOException {
+        return new YouTube.Builder(
+                new NetHttpTransport(),
+                JacksonFactory.getDefaultInstance(),
+                null)
+                .setApplicationName("API code samples")
+                .build();
+    }
+
+    public static void listCaptions(YouTube youtubeService, String videoId) throws IOException {
+        YouTube.Captions.List request = youtubeService.captions().list("snippet", videoId);
+        request.setKey(API_KEY); // Set the API key here
+
+        CaptionListResponse response = request.execute();
+        List<Caption> captions = response.getItems();
+
+        if (captions.isEmpty()) {
+            System.out.println("No captions found.");
+        } else {
+            System.out.println("Captions:");
+            for (Caption caption : captions) {
+                System.out.println(caption.getSnippet().getName());
+            }
+        }
     }
 }
